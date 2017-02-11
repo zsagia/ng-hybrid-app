10fcbdcac7844c2d688d717f8f48150744854f0a
mgmarlow <mgmarlow@gmail.com>
2017-02-11 14:42:56 -0800

    Move tabs around to highlight todo app

--- src/ng1-app/ng1-app.module.js
+++ src/ng1-app/ng1-app.module.js
@@ -1,14 +1,14 @@
 angular.module('ng1App', ['ui.router'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('main', {
-        url: '/',
-        component: 'ng1Main'
-      })
       .state('todoPage', {
         url: '/todo',
         component: 'todoPage'
+      })
+      .state('other', {
+        url: '/other',
+        component: 'ng1Main'
       });
 
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/todo');
   });
